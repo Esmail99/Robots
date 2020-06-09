46a4143e0bc4e66d5c659e74d20d32e086325779
Esmail <elmoussel12@gmail.com>
2020-06-09 21:45:47 +0200

    improve PWA & Accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const SearchBox = ({searchChange}) => {
-	return (
-		<div className='pa2'>
-			<input 
-				className='pa3 ba b--green bg-lightest-blue' 
-				type='search' 
-				placeholder='Search Robots' 
-				onChange={ searchChange } 
-			/>
-		</div>
-	);
-}
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="Search Robots"
+        onChange={searchChange}
+        label="search robots"
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
